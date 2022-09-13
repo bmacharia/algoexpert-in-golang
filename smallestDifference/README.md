195477aa8960fb3a3e02eb875aa78695c60fe25d
Babu Macharia <babu.macharia@gmail.com>
2022-09-13 12:27:27 -0700

    Solution to the Smallest Difference

---
+++ smallestDifference/README.md
@@ -0,0 +1,9 @@
+## Smallest Difference
+
+https://www.algoexpert.io/questions/smallest-difference
+
+Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from the first array in the first position.
+
+Note the absolute difference if two integers is the distance between them on the real number line. For example, the absolute difference of `-5` and `5` is `10`, and the absolute difference of `-5` and `-4` is `1`.
+
+You can assume that there will be one pair of numbers with the smallest difference.
---
+++ smallestDifference/smallestDiff.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func smallesDifference(arrayOne, arrayTwo []int) []int {
+	// sort the input arrays
+	sort.Ints(arrayOne)
+	sort.Ints(arrayTwo)
+	//intialize two pointers
+	idxOne, idxTwo := 0, 0
+	// initialize the smallest difference with the max value
+	smallest := math.MaxInt32
+	// initialize the current difference with the max value
+	current := math.MaxInt32
+	// create a slice to store the result
+	smallestPair := []int{}
+	//
+	for idxOne < len(arrayOne) && idxTwo < len(arrayTwo) {
+		// get the first element from the first array and the second element from the second array
+		firstNum, secondNum := arrayOne[idxOne], arrayTwo[idxTwo]
+		// if firtNum is less than secondNum
+		if firstNum < secondNum {
+			// set the current to the difference between the secondNum and the firstNum
+			current = secondNum - firstNum
+			// increment the idxOne
+			idxOne++
+			// else if the secondNum is less than the firstNum
+		} else if secondNum < firstNum {
+			// set the current to the difference between the firstNum and the secondNum
+			current = firstNum - secondNum
+			//
+			idxTwo++
+		} else {
+			// return the pair if firstNum and secondNum are equal
+			return []int{firstNum, secondNum}
+		}
+		// if the current is less than the smallest
+		if smallest > current {
+			// set the smallest to the current
+			smallest = current
+			// set the smallestPair to the pair of firstNum and secondNum
+			smallestPair = []int{firstNum, secondNum}
+		}
+	}
+	// return the smallestPair
+	return smallestPair
+}
+
+func main() {
+	arrayOne := []int{-1, 5, 10, 20, 28, 3}
+	arrayTwo := []int{26, 134, 135, 15, 17}
+	result := smallesDifference(arrayOne, arrayTwo)
+	fmt.Println(result)
+
+}
