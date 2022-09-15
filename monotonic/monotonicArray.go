c0399cee9dd37bf26e69ab594fc7dc4fa6018441
Babu Macharia <babu.macharia@gmail.com>
2022-09-15 12:25:48 -0700

    Add explanatory commnents to source code

--- monotonic/monotonicArray.go
+++ monotonic/monotonicArray.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func isMonotonic(array []int) bool {
+	// non-decreasing means the integer values in the array are increasing
 	isNonDecreasing := true
+	// non-increasing means the integer values are in the array are decreasing
 	isNonIncreasing := true
-
+	// iterate through the array starting from the second element
 	for i := 1; i < len(array); i++ {
+		// if the current element is less than the previous element, the interger values are trending downwards
 		if array[i] < array[i-1] {
 			isNonDecreasing = false
 		}
+		// if the current element is greater than the previous element, the interger values are trending upwards
 		if array[i] > array[i-1] {
 			isNonIncreasing = false
 		}
